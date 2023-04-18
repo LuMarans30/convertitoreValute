9bb80dff85c6feb43409493f46aa1ff0db214189
utente <utente>
2023-04-18 10:47:09 +0200

    pwa integration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,5 @@ export default defineConfig({
       },
     }
   },
-  plugins: [preact()],
+  plugins: [preact(), VitePWA()],
 });
