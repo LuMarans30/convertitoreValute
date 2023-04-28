06861321ec6b059ff13363a9c663997a46439d1c
LuMarans30 <andreamarano2004@gmail.com>
2023-04-28 17:25:47 +0200

    google login

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,25 @@
 import { Card, CardContent } from '@mui/material'
+
 import './app.css'
 import LoginAppBar from './components/loginappbar'
 import ConvertitoreForm from './components/convertitoreform'
 import ThemeManager from './components/utils/thememanager'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export function App() {
 
   return (
     <>
-      <ThemeManager>
-        <LoginAppBar />
-        <Card sx={{borderRadius: 10}}>
-          <CardContent>
-            <ConvertitoreForm />
-          </CardContent>
-        </Card>
-      </ThemeManager>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
+        <ThemeManager>
+          <LoginAppBar />
+          <Card sx={{ borderRadius: 10 }}>
+            <CardContent>
+              <ConvertitoreForm />
+            </CardContent>
+          </Card>
+        </ThemeManager>
+      </GoogleOAuthProvider>
     </>
   )
 }
--- src/components/loginappbar.jsx
+++ src/components/loginappbar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { AppBar, Box, Toolbar, Typography, IconButton, Button } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+
+import MenuIcon  from '@mui/icons-material/Menu';
+import GoogleIcon from '@mui/icons-material/Google';
+
+import { useGoogleLogin } from '@react-oauth/google';
 
 export default function LoginAppBar() {
+
+    const login = useGoogleLogin({
+        onSuccess: credentialResponse => {
+            console.log(credentialResponse);
+        },
+        onError: () => {
+            console.log('Login Failed');
+        },
+    });
+
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
@@ -20,7 +34,15 @@ export default function LoginAppBar() {
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                             Convertitore valute
                         </Typography>
-                        <Button color="inherit">Login</Button>
+                        <Button
+                            type="button"
+                            color="secondary"
+                            variant="contained"
+                            startIcon={<GoogleIcon />}
+                            onClick={() => login()}
+                        >
+                            Login with Google
+                        </Button>
                     </Toolbar>
                 </AppBar>
             </Box>
